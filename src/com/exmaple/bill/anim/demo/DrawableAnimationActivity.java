60b53a20dc3641ea3288ad3d9504a3f7fab36484
lber19535 <lber19535@126.com>
2015-04-05 12:36:16 +0800

    [Bill Lv] drawable 动画

---
+++ src/com/exmaple/bill/anim/demo/DrawableAnimationActivity.java
@@ -0,0 +1,42 @@
+package com.exmaple.bill.anim.demo;
+
+import com.exmaple.bill.R;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class DrawableAnimationActivity extends Activity {
+    
+    private AnimationDrawable mAnimationDrawable;
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable_anim);
+        
+        ImageView animImageView = (ImageView) findViewById(R.id.drawable_anim);
+        animImageView.setBackground(getResources().getDrawable(R.drawable.drawable_anim));
+        mAnimationDrawable = (AnimationDrawable) animImageView.getBackground();
+        
+        findViewById(R.id.start).setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                mAnimationDrawable.start();
+            }
+        });
+        
+        findViewById(R.id.stop).setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                mAnimationDrawable.stop();
+            }
+        });
+    }
+}
