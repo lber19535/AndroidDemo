473795eefb222870449ecff8e1eea0d73b3ca68e
lber19535 <lber19535@126.com>
2015-04-05 14:22:38 +0800

    [Bill Lv] drawable demo补充

--- src/com/exmaple/bill/anim/demo/DrawableAnimationActivity.java
+++ src/com/exmaple/bill/anim/demo/DrawableAnimationActivity.java
@@ -9,8 +9,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
+/**
+ * Drawable Animation
+ * 
+ * @author Bill-pc
+ *
+ */
 public class DrawableAnimationActivity extends Activity {
-    
+
     private AnimationDrawable mAnimationDrawable;
 
     @SuppressLint("NewApi")
@@ -18,25 +24,33 @@ public class DrawableAnimationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawable_anim);
-        
+
         ImageView animImageView = (ImageView) findViewById(R.id.drawable_anim);
-        animImageView.setBackground(getResources().getDrawable(R.drawable.drawable_anim));
+        animImageView.setBackground(getResources().getDrawable(
+                R.drawable.drawable_anim));
         mAnimationDrawable = (AnimationDrawable) animImageView.getBackground();
-        
+
         findViewById(R.id.start).setOnClickListener(new OnClickListener() {
-            
+
             @Override
             public void onClick(View v) {
                 mAnimationDrawable.start();
             }
         });
-        
+
         findViewById(R.id.stop).setOnClickListener(new OnClickListener() {
-            
+
             @Override
             public void onClick(View v) {
                 mAnimationDrawable.stop();
             }
         });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        // start()无法在onCreate中使用
+        mAnimationDrawable.start();
     }
 }
