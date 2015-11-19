611ea720b73057db4ca92f33cdc92379da9e9caf
Bill_lv <lber19535@126.com>
2015-11-19 17:43:05 +0800

    [Bill Lv] databinding 部分代码

---
+++ app/src/main/java/com/exmaple/bill/designpattern/mvvm/model/ObservableUser.java
@@ -0,0 +1,61 @@
+package com.exmaple.bill.designpattern.mvvm.model;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.exmaple.bill.BR;
+
+
+/**
+ * Created by bill_lv on 2015/11/15.
+ */
+public class ObservableUser extends BaseObservable {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    ;
+
+    public ObservableUser(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Bindable
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
+    }
+
+    @Bindable
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+        notifyPropertyChanged(BR.lastName);
+    }
+
+    public boolean isAdult() {
+        if (age > 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Bindable
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        notifyPropertyChanged(BR._all);
+    }
+}
---
+++ app/src/main/java/com/exmaple/bill/designpattern/mvvm/ui/ActivityResource.java
@@ -0,0 +1,7 @@
+package com.exmaple.bill.designpattern.mvvm.ui;
+
+/**
+ * Created by bill_lv on 2015/11/19.
+ */
+public class ActivityResource {
+}
