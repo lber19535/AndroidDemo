d48426eba5250112ed4443d49b3a876bb69c651b
bill_lv <lber19535@126.com>
2015-11-23 09:56:33 +0800

    [Bill Lv] update demo

---
+++ app/src/main/java/com/exmaple/bill/databinding/ui/ActivityConversion.java
@@ -0,0 +1,38 @@
+package com.exmaple.bill.databinding.ui;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.exmaple.bill.R;
+import com.exmaple.bill.databinding.ActivityDatabindingConversionBinding;
+
+/**
+ * Created by Bill Lv on 2015/11/21.
+ */
+public class ActivityConversion extends AppCompatActivity {
+
+    ActivityDatabindingConversionBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_databinding_conversion);
+
+    }
+
+    public void onBackgroundColorChanged(View v) {
+        if (binding.getError()) {
+            binding.setError(false);
+        } else {
+            binding.setError(true);
+        }
+    }
+
+//    @BindingConversion
+//    public static Drawable converColor(int color){
+//        System.out.println("xxxxxx" + color);
+//        return new ColorDrawable(color & 0x10101010);
+//    }
+}
