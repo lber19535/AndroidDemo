393599525b71989a208d06e3ee6bf67de232eb34
Bill Lv <lber19535@126.com>
2017-02-10 14:53:50 +0800

    add JobScheduler

--- app/src/main/java/com/example/bill/app/service/ActivityJobSheduler.java
+++ app/src/main/java/com/example/bill/app/service/ActivityJobSheduler.java
@@ -3,8 +3,11 @@ package com.example.bill.app.service;
 import android.annotation.TargetApi;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -17,23 +20,41 @@ import com.example.bill.R;
 public class ActivityJobSheduler extends AppCompatActivity {
 
     private static final String TAG = "ActivityJobSheduler";
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            System.out.println("screen off");
+            JobScheduler scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+            JobInfo.Builder builder = new JobInfo.Builder(1, new ComponentName(getPackageName(), MyJobService.class.getName()));
+            builder.setPeriodic(3000);
+            if (scheduler.schedule(builder.build()) <= 0) {
+                Log.d(TAG, "onClick: something wrong");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_sheduler);
 
+        IntentFilter screenStateFilter = new IntentFilter();
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+
         findViewById(R.id.job_btn).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                JobScheduler scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-                JobInfo.Builder builder = new JobInfo.Builder(1, new ComponentName(getPackageName(), MyJobService.class.getName()));
-                builder.setPeriodic(3000);
-                if (scheduler.schedule(builder.build()) <= 0){
-                    Log.d(TAG, "onClick: something wrong");
-                }
+
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
     }
 }
--- app/src/main/java/com/example/bill/app/service/MyJobService.java
+++ app/src/main/java/com/example/bill/app/service/MyJobService.java
@@ -1,9 +1,19 @@
 package com.example.bill.app.service;
 
 import android.annotation.TargetApi;
+import android.app.Service;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.IntDef;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Bill on 2016/11/14.
@@ -14,6 +24,14 @@ public class MyJobService extends JobService {
     @Override
     public boolean onStartJob(JobParameters params) {
         System.out.println("on start job");
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
+        scheduledExecutorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("xxxxxx");
+            }
+        }, 3, TimeUnit.SECONDS);
         return false;
     }
 
@@ -21,5 +39,11 @@ public class MyJobService extends JobService {
     public boolean onStopJob(JobParameters params) {
         System.out.println("on stop job");
         return false;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("qqqqqqqqqqqq");
+        return super.onStartCommand(intent, flags, startId);
     }
 }
